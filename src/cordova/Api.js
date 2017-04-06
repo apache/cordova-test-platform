0ffe8a53439196f8f938340f875765e7c11bde25
Jesse MacFadyen <purplecabbage@gmail.com>
2017-04-05 17:24:47 -0700

    updated paths, added config handling, cordova-lib tests passing, uses events to log

--- src/cordova/Api.js
+++ src/cordova/Api.js
@@ -8,6 +8,9 @@
 var shell = require('shelljs');
 var path = require('path');
 
+var CordovaLogger = require('cordova-common').CordovaLogger;
+var selfEvents = require('cordova-common').events;
+
 var PLATFORM_NAME = 'testplatform';
 
 function setupEvents(externalEventEmitter) {
@@ -46,25 +49,32 @@ function Api(platform, platformRootDir, events) {
 
 Api.createPlatform = function (destination, config, options, events) {
 
+    events = setupEvents(events);
+
     // create the destination and the standard place for our api to live
     // platforms/platformName/cordova/Api.js
-    shell.mkdir(destination);
-    shell.mkdir(path.join(destination,'cordova'));
 
-    var apiPath = path.join(__dirname, '**'); // default value
+    var apiSrcPath = __dirname; // default value
     // does options contain the info we desire?
-    if(options && options.platformDetails) {
-        apiPath = path.join(options.platformDetails.libDir, '**');
-    }
+
+    var projectName = config ? config.name() : "HelloCordova";
+
+    events.emit('log', 'Creating Cordova project for cordova-platform-test:');
+    events.emit('log', '\tPath: ' + destination);
+    events.emit('log', '\tName: ' + projectName);
+
+    shell.mkdir(destination);
 
     // move a copy of our api to the new project
-    shell.cp(apiPath, path.join(destination, 'cordova'));
+    shell.cp('-r',apiSrcPath, destination);
 
-    var result = Promise.resolve();
-    result.then(function(){
-       return new Api(PLATFORM_NAME,destination,events);
-    });
-    return result;
+    // move our node_modules
+    var srcModulePath = path.join(__dirname,'../../node_modules');
+    shell.cp('-r',srcModulePath,destination);
+
+    // I promise I will return
+    return Promise.resolve(new Api(PLATFORM_NAME,destination,events));
+
 };
 
 
